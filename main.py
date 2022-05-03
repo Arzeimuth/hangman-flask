c4072ac6a1fbbd047949ede57f5ffc992e84ef64
Brandon Burton <bburton.cg@gmail.com>
2022-05-03 00:02:15 -0700

    Realtime yeah

--- main.py
+++ main.py
@@ -1,50 +1,47 @@
-from aiohttp import web
-import socketio
-import os
-from flask import Flask
+from flask import Flask, render_template, session, copy_current_request_context
+from flask_socketio import SocketIO, emit, disconnect
+from threading import Lock
+
+
+async_mode = None
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socket_ = SocketIO(app, async_mode=async_mode)
+thread = None
+thread_lock = Lock()
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html', async_mode=socket_.async_mode)
+
+
+@socket_.on('my_event', namespace='/test')
+def test_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': message['data'], 'count': session['receive_count']})
+
+
+@socket_.on('my_broadcast_event', namespace='/test')
+def test_broadcast_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': message['data'], 'count': session['receive_count']},
+         broadcast=True)
+
+
+@socket_.on('disconnect_request', namespace='/test')
+def disconnect_request():
+    @copy_current_request_context
+    def can_disconnect():
+        disconnect()
+
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': 'Disconnected!', 'count': session['receive_count']},
+         callback=can_disconnect)
+
 
 if __name__ == '__main__':
-    app.run()
-
-
-
-# ## creates a new Async Socket IO Server
-# sio = socketio.AsyncServer()
-# ## Creates a new Aiohttp Web Application
-# app = web.Application()
-# # Binds our Socket.IO server to our Web App
-# ## instance
-# sio.attach(app)
-
-# ## we can define aiohttp endpoints just as we normally
-# ## would with no change
-# async def index(request):
-#     with open('index.html') as f:
-#         with open('gamefile.json') as game:
-
-#             return web.Response(text=f.read(), content_type='text/html')
-
-
-# ## If we wanted to create a new websocket endpoint,
-# ## use this decorator, passing in the name of the
-# ## event we wish to listen out for
-# @sio.on('message')
-# async def print_message(sid, message):
-#     print("Socket ID: " , sid)
-#     print(message)
-#     ## await a successful emit of our reversed message
-#     ## back to the client
-#     await sio.emit('message', message)
-
-# ## We bind our aiohttp endpoint to our app
-# ## router
-# app.router.add_get('/', index)
-
-# ## We kick off our server
-# if __name__ == '__main__':
-#     web.run_app(app)
+    socket_.run(app, debug=True)
